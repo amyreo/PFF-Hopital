751c520ed752930cffe6b5189b58287b16308c19
jonathan <jonathan@BKE592H>
2022-12-20 11:16:29 +0100

    rectification erreur

--- src/main/java/org/mycompany/model/Ordonance.java
+++ src/main/java/org/mycompany/model/Ordonance.java
@@ -16,10 +16,6 @@ public class Ordonance {
             inverseJoinColumns = @JoinColumn( name = "idMedi" ) )
 	  private List<Medicament> medicaments = new ArrayList<>();
 	
-	   @ManyToMany  @JoinColumn( name="idMede" )
-	    private Medecin medecin;
-	@JoinTable(name = "T_Ordo_Medi_Associations", joinColumns = @JoinColumn(name = "idOrdo"), inverseJoinColumns = @JoinColumn(name = "idMedi"))
-	private List<Medicament> medicaments = new ArrayList<>();
 
 	@ManyToMany
 	@JoinColumn(name = "idMede")
