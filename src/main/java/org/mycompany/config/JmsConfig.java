6983d586655d3e5ed0b7e0f1d78e7aa6fbbcfaa9
theolorenzi <theo.lorenzi@laposte.net>
2022-12-23 17:01:19 +0100

    Modifications JmsConfig pour éviter erreurs Modifications mainMenu pour mettre à jour Patient et Medecin après insertion RDV

--- src/main/java/org/mycompany/config/JmsConfig.java
+++ src/main/java/org/mycompany/config/JmsConfig.java
@@ -1,11 +1,14 @@
 package org.mycompany.config;
 
 import javax.jms.ConnectionFactory;
+import javax.persistence.EntityManagerFactory;
 
 import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.JmsTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JmsConfig {
@@ -21,6 +24,14 @@ public class JmsConfig {
 			final JmsTransactionManager jmsTransactionManager) {
 		JmsComponent jmsComponent = JmsComponent.jmsComponentTransacted(connectionFactory, jmsTransactionManager);
 		return jmsComponent;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
 	}
 
 }
--- src/main/java/org/mycompany/controller/GlobalController.java
+++ src/main/java/org/mycompany/controller/GlobalController.java
@@ -42,22 +42,31 @@ public class GlobalController {
 
 		int choice = 0;
 		do {
-			System.err.println(
+			System.out.println("");
+			System.out.println(
 					"Tapez 1 pour ajouter un patient, 2 pour lister tous les patients, 3 pour voir un patient en particulier, 4 pour supprimer un patient.");
+			System.out.println("");
 			System.out.println(
 					"Tapez 5 pour ajouter un médecin, 6 pour lister tous les médecins, 7 pour voir un médecin en particulier, 8 pour supprimer un médecin.");
-			System.err.println(
+			System.out.println("");
+			System.out.println(
 					"Tapez 9 pour ajouter une chambre, 10 pour lister toutes les chambres, 11 pour voir une chambre en particulier, 12 pour supprimer une chambre.");
+			System.out.println("");
 			System.out.println(
 					"Tapez 13 pour ajouter une facture, 14 pour lister toutes les factures, 15 pour voir une facture en particulier, 16 pour supprimer une facture.");
-			System.err.println(
+			System.out.println("");
+			System.out.println(
 					"Tapez 17 pour ajouter une ordonnance, 18 pour lister toutes les ordonnances, 19 pour voir une ordonnance en particulier, 20 pour supprimer une ordonnance.");
+			System.out.println("");
 			System.out.println("Tapez 21 pour planifier un RDV, 22 pour consulter le planning des RDV.");
-			System.err.println(
+			System.out.println("");
+			System.out.println(
 					"Pour ajouter un objet en tant qu'attribut d'une autre objet, veuillez d'abord créer les deux objets séparément.");
+			System.out.println("");
 			choice = scan.nextInt();
 			switch (choice) {
 			case 1:
+				System.out.println("");
 				System.out.println("Entrez l'identifiant du patient : ");
 				int id = scan.nextInt();
 				System.out.println("Entrez le nom du patient : ");
@@ -72,23 +81,26 @@ public class GlobalController {
 				break;
 
 			case 2:
+				System.out.println("");
 				System.out.println("Allez à l'adresse suivante svp : ");
 				System.err.println("localhost:8080/getPatients");
 				break;
 			case 3:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant du patient que vous souhaitez visualiser ?");
 				int id2 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/getPatient/" + id2);
 				break;
 			case 4:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant du patient que vous souhaitez supprimer ?");
 				int id3 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/deletePatient/" + id3);
 				break;
 			case 5:
-
+				System.out.println("");
 				System.out.println("Entrez l'identifiant du médecin : ");
 				int idm = scan.nextInt();
 				System.out.println("Entrez le nom du médecin : ");
@@ -102,22 +114,26 @@ public class GlobalController {
 				System.out.println("Le médecin a bien été enregistré.");
 				break;
 			case 6:
+				System.out.println("");
 				System.out.println("Allez à l'adresse suivante svp : ");
 				System.err.println("localhost:8080/getMedecins");
 				break;
 			case 7:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant du médecin que vous souhaitez visualiser ?");
 				int id4 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/getMedecin/" + id4);
 				break;
 			case 8:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant du médecin que vous souhaitez supprimer ?");
 				int id5 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/deleteMedecin/" + id5);
 				break;
 			case 9:
+				System.out.println("");
 				System.out.println("Entrez l'identifiant de la chambre : ");
 				int idc = scan.nextInt();
 				System.out.println("Entrez la taille de la chambre en m2 (entiers seulement) : ");
@@ -127,22 +143,26 @@ public class GlobalController {
 				System.out.println("La chambre a bien été enregistrée.");
 				break;
 			case 10:
+				System.out.println("");
 				System.out.println("Allez à l'adresse suivante svp : ");
 				System.err.println("localhost:8080/getChambres");
 				break;
 			case 11:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de la chambre que vous souhaitez visualiser ?");
 				int id6 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/getChambre/" + id6);
 				break;
 			case 12:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de la chambre que vous souhaitez supprimer ?");
 				int id7 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/deleteChambre/" + id7);
 				break;
 			case 13:
+				System.out.println("");
 				System.out.println("Entrez l'identifiant de la facture : ");
 				int idf = scan.nextInt();
 				System.out.println("Entrez le montant de la facture : ");
@@ -152,22 +172,26 @@ public class GlobalController {
 				System.out.println("La facture a bien été enregistrée.");
 				break;
 			case 14:
+				System.out.println("");
 				System.out.println("Allez à l'adresse suivante svp : ");
 				System.err.println("localhost:8080/getFactures");
 				break;
 			case 15:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de la facture que vous souhaitez visualiser ?");
 				int id8 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/getFacture/" + id8);
 				break;
 			case 16:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de la facture que vous souhaitez supprimer ?");
 				int id9 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/deleteFacture/" + id9);
 				break;
 			case 17:
+				System.out.println("");
 				System.out.println("Entrez l'identifiant de l'ordonnance : ");
 				int ido = scan.nextInt();
 				Ordonance ord = new Ordonance(ido, null);
@@ -175,22 +199,26 @@ public class GlobalController {
 				System.out.println("L'ordonnance a bien été enregistrée.");
 				break;
 			case 18:
+				System.out.println("");
 				System.out.println("Allez à l'adresse suivante svp : ");
 				System.err.println("localhost:8080/getOrdonances");
 				break;
 			case 19:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de l'ordonnance que vous souhaitez visualiser ?");
 				int id10 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/getOrdonance/" + id10);
 				break;
 			case 20:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant de l'ordonnance que vous souhaitez supprimer ?");
 				int id11 = scan.nextInt();
 				System.out.println("Rendez-vous à l'adresse suivante : ");
 				System.err.println("localhost:8080/deleteOrdonance/" + id11);
 				break;
 			case 21:
+				System.out.println("");
 				System.out.println("Quel est l'identifiant du RDV à créer ?");
 				int idrdv = scan.nextInt();
 				System.out.println("Pour quelle maladie prenez-vous RDV ?");
@@ -209,7 +237,7 @@ public class GlobalController {
 				Patient patient2 = ipr.findById(idP).get();
 				Medecin medC = imr.findById(idM).get();
 				boolean dispo = true;
-				if (medC.getListeRdv().size() >= 1) {
+				if (medC.getListeRdv() != null) {
 					List<RDV> listeR = medC.getListeRdv();
 					for (RDV rdv : listeR) {
 						if (rdv.getHeureDebut() == heure) {
@@ -223,6 +251,18 @@ public class GlobalController {
 					facture.setRdv(rdv);
 					ifr.save(facture);
 					irr.save(rdv);
+					List<RDV> listeR = patient2.getListeRDV();
+					listeR.add(rdv);
+					List<RDV> listeRM = medC.getListeRdv();
+					listeRM.add(rdv);
+					List<Patient> listeP = medC.getListePatients();
+					listeP.add(patient2);
+					List<Medecin> listeM = patient2.getListeMedecins();
+					listeM.add(medC);
+
+					ipr.save(patient2);
+					imr.save(medC);
+
 				} else {
 					System.out.println(
 							"Le médecin n'est pas disponible à cette heure-ci, veuillez recommencer le processus de prise de RDV.");
@@ -230,6 +270,7 @@ public class GlobalController {
 
 				break;
 			case 22:
+				System.out.println("");
 				System.out.println("Voici la liste des RDV qui sont déjà programmés : ");
 				List<RDV> listeRDV = irr.findAll();
 				for (RDV rdv : listeRDV) {
